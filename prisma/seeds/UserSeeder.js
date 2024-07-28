585a1fa0d302778784a057ddb29971caf87b46cb
Nay Ba La <naybala.zotefamily@gmail.com>
2024-07-28 11:23:03 +0630

    add faker

--- prisma/seeds/UserSeeder.js
+++ prisma/seeds/UserSeeder.js
@@ -1,10 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
+const { faker } = require("@faker-js/faker");
 const bcrypt = require("bcrypt");
 
 const prisma = new PrismaClient();
 
 async function UserSeeder() {
-  console.log("%c User seeding started...", "color:blue;");
+  console.log("User seeding started...");
   for (let i = 0; i < 10; i++) {
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
@@ -12,13 +13,14 @@ async function UserSeeder() {
     const username = `${firstName}${lastName[0]}`.toLocaleLowerCase();
     const password = bcrypt.hash(`password${i}`, 10);
     const bio = faker.person.bio();
-    await prisma.user.upsert({
-      where: { username },
-      update: {},
-      create: { name, username, bio, password },
-    });
+    console.log(name, username, bio, password);
+    // await prisma.user.upsert({
+    //   where: { username },
+    //   update: {},
+    //   create: { name, username, bio, password },
+    // });
   }
-  console.log("%c User seeding was done.", "color:green;");
+  console.log("User seeding was done.");
 }
 
 module.exports = { UserSeeder };
