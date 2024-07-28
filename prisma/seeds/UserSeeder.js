5704df05d09dfd2e2143ae311c6fdbbfb33117c9
Nay Ba La <naybala.zotefamily@gmail.com>
2024-07-28 11:20:09 +0630

    modify: User Seeder
    demo data for user seeder

--- prisma/seeds/UserSeeder.js
+++ prisma/seeds/UserSeeder.js
@@ -4,9 +4,21 @@ const bcrypt = require("bcrypt");
 const prisma = new PrismaClient();
 
 async function UserSeeder() {
-  const password = await bcrypt.hash("password", 10);
-  console.log(password);
-  console.log("User seeding started...");
+  console.log("%c User seeding started...", "color:blue;");
+  for (let i = 0; i < 10; i++) {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const name = `${firstName} ${lastName}`;
+    const username = `${firstName}${lastName[0]}`.toLocaleLowerCase();
+    const password = bcrypt.hash(`password${i}`, 10);
+    const bio = faker.person.bio();
+    await prisma.user.upsert({
+      where: { username },
+      update: {},
+      create: { name, username, bio, password },
+    });
+  }
+  console.log("%c User seeding was done.", "color:green;");
 }
 
 module.exports = { UserSeeder };
